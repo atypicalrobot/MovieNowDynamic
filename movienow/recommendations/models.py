1a9b021468d549e0490473a0c8c2e4602754ee64
Tania Manoli <github@atypicalrobot.com>
2016-05-25 21:56:52 +0100

    Fix editable=True for recommendation.created_by

--- movienow/recommendations/models.py
+++ movienow/recommendations/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Recommendation(models.Model):
     movie_id = models.CharField(max_length=255)
     rating = models.IntegerField()
-    created_by = models.ForeignKey(User, related_name='recommends', editable=False)
+    created_by = models.ForeignKey(User, related_name='recommends', editable=True)
 
     def __str__(self):
         return '{} rated {} by {}'.format(self.movie_id, self.rating, self.created_by)
